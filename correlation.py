cc58eb386b1b4020b04a3bae38679f5896d32c77
Alessio <randomcoil@protonmail.com>
2025-12-16 12:19:08 +0100

    Examples and supporting scripts are now ok for Python3

--- correlation.py
+++ correlation.py
@@ -13,27 +13,27 @@ X, sup = ar.readarff("data/highdataproclus.arff")
 
 NRUNS = 15
 
-objs = np.zeros(NRUNS) # objective function results
-adjs = np.zeros(NRUNS) # adjusted rand index results
+objs = np.zeros(NRUNS)  # objective function results
+adjs = np.zeros(NRUNS)  # adjusted rand index results
 
-print "Beginning runs..."
+print("Beginning runs...")
 
-for i in xrange(NRUNS):
-	rseed = np.random.randint(low = 0, high = 1239831)
-	print ">>> Run %d/%d using seed %d" % (i + 1, NRUNS, rseed)
-	M, D, A = prc.proclus(X, k = 7, l = 2, seed = rseed)
-	objs[i] = prc.evaluateClusters(X, A, D, M)
-	adjs[i] = adjrand.computeAdjustedRandIndex(A, sup)
+for i in range(NRUNS):
+    rseed = np.random.randint(low=0, high=1239831)
+    print(">>> Run %d/%d using seed %d" % (i + 1, NRUNS, rseed))
+    M, D, A = prc.proclus(X, k=7, l=2, seed=rseed)
+    objs[i] = prc.evaluateClusters(X, A, D, M)
+    adjs[i] = adjrand.computeAdjustedRandIndex(A, sup)
 
-print "Finished runs..."
+print("Finished runs...")
 
 sidx = np.argsort(objs)
 
 plt.clf()
-plt.plot(objs[sidx], adjs[sidx], 'bo-')
+plt.plot(objs[sidx], adjs[sidx], "bo-")
 plt.xlabel("Objective function results")
 plt.ylabel("Adjusted Rand Index")
 plt.show()
 plt.draw()
 
-print "Pearson correlation: %.4f" % pearsonr(objs[sidx], adjs[sidx])[0]
+print("Pearson correlation: %.4f" % pearsonr(objs[sidx], adjs[sidx])[0])
